d4fcafe21df06df951df277aed7f32f7aca2ba20
Joonwan Kim <joonwk@gmail.com>
2023-08-02 12:29:21 +0900

    link 수정

--- components/header.tsx
+++ components/header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
   return (
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="https://https://amiailab.vercel.app/">
+        <Navbar.Brand href="https://amiailab.vercel.app/">
           {'AmiAiLab '}
         </Navbar.Brand>
 
